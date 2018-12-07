f7689769c05db3bd3ecd480caf812829e38718c4
minhduc <duc.hoang@playnext-lab.co.jp>
2018-12-07 17:52:36 +0900

    deploy_to_heroku

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +16,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(compression());
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,7 +30,7 @@ app.use('/catalog', catalogRouter);
 
 //connect database
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://127.0.0.1/express_locallibary_tutorial';
+var mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1/express_locallibary_tutorial';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
